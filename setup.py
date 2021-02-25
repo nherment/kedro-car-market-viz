0c94f3d182a352dca7de558354274e4e26601fdb
Nicolas Herment <nherment@macbook.local>
2021-02-25 10:05:27 +0100

    test initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='kedro-car-market-viz',
+    version='0.0.1',
+    description='A library to view analysis of the sales information of cars',
+    license='Apache-2',
+    packages=['kedro-car-market-viz'],
+    author='Nicolas Herment',
+    author_email='nicolas.herment@nearform.com',
+    keywords=['kedro', 'visualization', 'cars'],
+    url='https://github.com/nherment/kedro-car-market-viz'
+)
+
